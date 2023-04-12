f4b31a9bb46fa2c3110a7e7ad21253f4104f06dd
webtask-eu <webtask.eu@gmail.com>
2023-04-12 09:26:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once 'auth.php';
 
 ?>
 
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,6 +18,13 @@ require_once 'auth.php';
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+  <style>
+    body {
+  font-family: 'Montserrat', sans-serif;
+}
+    </style>
 </head>
 <body>
 
