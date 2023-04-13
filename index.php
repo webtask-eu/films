49a3675373486579b302e3a031c53cdbd2d0a403
webtask-eu <webtask.eu@gmail.com>
2023-04-13 11:57:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -43,59 +43,53 @@ if (isset($_POST['login'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Movies Collection</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Movies Collection</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <style>
+        .card {
+            margin-bottom: 20px;
+        }
+        .card-header {
+            font-size: 20px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">Movies Collection</h1>
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <?php if (isset($error)): ?>
-                    <div class="alert alert-danger"><?= $error ?></div>
-                <?php endif; ?>
+            <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">
-                        Login
-                    </div>
+                    <div class="card-header">Register or Login</div>
                     <div class="card-body">
-                        <form method="post">
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" class="form-control" id="email" name="email" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary" name="login">Login</button>
-                        </form>
+                        <a href="register.php" class="btn btn-primary">Register</a>
+                        <a href="login.php" class="btn btn-secondary">Login</a>
                     </div>
                 </div>
-                <div class="card mt-4">
-                    <div class="card-header">
-                        Register
-                    </div>
+                <div class="card">
+                    <div class="card-header">Users with collections</div>
                     <div class="card-body">
-                        <form method="post">
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" class="form-control" id="email" name="email" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="confirm_password">Confirm password</label>
-                                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary" name="register">Register</button>
-                        </form>
+                        <?php
+                        require_once 'controller.php';
+
+                        $controller = new Controller();
+
+                        $collections = $controller->getUserCollectionsList();
+                        if (count($collections) > 0) {
+                            echo '<ul class="list-group">';
+                            foreach ($collections as $collection) {
+                                echo '<li class="list-group-item">';
+                                echo '<a href="collection.php?id=' . $collection['id'] . '">' . $collection['title'] . '</a>';
+                                echo '</li>';
+                            }
+                            echo '</ul>';
+                        } else {
+                            echo '<p>No collections found</p>';
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
@@ -103,4 +97,5 @@ if (isset($_POST['login'])) {
     </div>
 </body>
 </html>
+
 
