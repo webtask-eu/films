63c5c166bdf2b6d00665ffdbec1d23f7dd19f04f
webtask-eu <webtask.eu@gmail.com>
2023-04-13 11:40:33 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+require_once 'controller.php';
+$controller = new Controller();
+
+// Если пользователь авторизован, перенаправляем его на страницу профиля
+if ($controller->isLoggedIn()) {
+    header('Location: profile.php');
+    exit();
+}
+
+// Обработка регистрации
+if (isset($_POST['register'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    $result = $controller->registerUser($email, $password, $confirm_password);
+    if ($result === true) {
+        // Если регистрация прошла успешно, перенаправляем пользователя на страницу профиля
+        header('Location: profile.php');
+        exit();
+    } else {
+        // Если при регистрации возникла ошибка, выводим ее на экран
+        $error = $result;
+    }
+}
+
+// Обработка авторизации
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $result = $controller->loginUser($email, $password);
+    if ($result === true) {
+        // Если авторизация прошла успешно, перенаправляем пользователя на страницу профиля
+        header('Location: profile.php');
+        exit();
+    } else {
+        // Если при авторизации возникла ошибка, выводим ее на экран
+        $error = $result;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Movies Collection</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Movies Collection</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <?php if (isset($error)): ?>
+                    <div class="alert alert-danger"><?= $error ?></div>
+                <?php endif; ?>
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form method="post">
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="login">Login</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="card mt-4">
+                    <div class="card-header">
+                        Register
+                    </div>
+                    <div class="card-body">
+                        <form method="post">
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <
