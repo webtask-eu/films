d1f15e2972dda3ba0a734d4f9c4a08887ce26e96
webtask-eu <webtask.eu@gmail.com>
2023-04-20 20:22:47 +0300

    r

--- register.php
+++ register.php
@@ -1,42 +1,22 @@
 <?php
 require_once 'db.php';
 
-// Проверяем, была ли отправлена форма регистрации
-if (isset($_POST['register'])) {
-
-    // Получаем данные из формы регистрации
+// Если форма была отправлена, то обрабатываем данные
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Хешируем пароль
-    $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+    // Создаем объект класса DB и регистрируем пользователя
+    $db = new DB();
+    $result = $db->query('INSERT INTO users (email, password) VALUES (?, ?)', array($email, $password));
 
-    // Подготавливаем SQL-запрос на проверку наличия пользователя с таким же email
-    $sql = "SELECT id FROM users WHERE email = :email LIMIT 1";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['email' => $email]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // Если пользователь с таким email уже есть, выводим ошибку
-    if ($user) {
-        echo "Пользователь с таким email уже зарегистрирован.";
-        exit();
+    // Если регистрация прошла успешно, то авторизуем пользователя
+    if ($result) {
+        session_start();
+        $_SESSION['user_id'] = $db->getLastInsertId();
+        header('Location: index.php');
+        exit;
     }
-
-    // Если пользователь с таким email не найден, добавляем его в базу данных
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['email' => $email, 'password' => $password_hashed]);
-
-    // Автоматически логиним пользователя после регистрации
-    $sql = "SELECT id FROM users WHERE email = :email LIMIT 1";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['email' => $email]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    $_SESSION['user_id'] = $user['id'];
-    header("Location: index.php");
-    exit();
 }
 ?>
 
@@ -44,16 +24,17 @@ if (isset($_POST['register'])) {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Регистрация нового пользователя</title>
+    <title>Registration</title>
 </head>
 <body>
-    <h1>Регистрация нового пользователя</h1>
-    <form method="POST">
-        <label>E-mail:</label>
-        <input type="email" name="email" required><br>
-        <label>Пароль:</label>
-        <input type="password" name="password" required><br>
-        <input type="submit" name="register" value="Зарегистрироваться">
-    </form>
+<h1>Registration</h1>
+<form method="post">
+    <label>Email:</label><br>
+    <input type="email" name="email" required><br>
+    <label>Password:</label><br>
+    <input type="password" name="password" required><br>
+    <br>
+    <button type="submit">Register</button>
+</form>
 </body>
 </html>
