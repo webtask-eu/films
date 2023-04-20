024dd914a0f1bb1b2c20163095e13ed07e2002db
webtask-eu <webtask.eu@gmail.com>
2023-04-20 21:10:21 +0300

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'db.php';
+require_once 'utils.php';
+
+session_start();
+
+// Проверяем, авторизован ли пользователь
+if (!isAuthorized()) {
+  header('Location: index.php');
+  exit();
+}
+
+// Получаем информацию о пользователе
+$db = new DB();
+$user = $db->query('SELECT * FROM users WHERE id = ?', array($_SESSION['user_id']));
+if (!$user) {
+  die('Failed to get user info');
+}
+$user = $user[0];
+
+// Выводим информацию о пользователе
+echo 'Welcome, ' . $user['name'] . '!<br>';
+echo 'Your email: ' . $user['email'] . '<br>';
+echo 'Your password: ' . $user['password'] . '<br>';
+
+// Форма для изменения пароля
+echo '<h2>Change password</h2>';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Обработка формы
+  $oldPassword = $_POST['oldPassword'];
+  $newPassword = $_POST['newPassword'];
+
+  // Проверяем, что старый пароль введен верно
+  if (password_verify($oldPassword, $user['password'])) {
+    // Обновляем пароль в базе данных
+    $newPasswordHash = password_hash($newPassword, PASSWORD_DEFAULT);
+    $result = $db->query('UPDATE users SET password = ? WHERE id = ?', array($newPasswordHash, $_SESSION['user_id']));
+    if ($result) {
+      echo 'Password changed successfully!';
+    } else {
+      echo 'Failed to change password. Please try again.';
+    }
+  } else {
+    echo 'Incorrect old password. Please try again.';
+  }
+}
+
+echo '<form method="post">';
+echo 'Old password: <input type="password" name="oldPassword"><br>';
+echo 'New password: <input type="password" name="newPassword"><br>';
+echo '<input type="submit" value="Change password">';
+echo '</form>';
