9eb0458ceaa878e6c5e8b8086929c76241134f16
webtask-eu <webtask.eu@gmail.com>
2023-04-25 13:43:54 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -51,36 +51,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 // Замените YOUR_API_KEY на свой API-ключ для The Movie Database API
 $api_key = 'fca80a35e9a4bccbf9a300c8e938e3e0';
 
-// Подключаем библиотеку для отправки HTTP-запросов
 require_once 'vendor/autoload.php';
 use GuzzleHttp\Client;
 
-// Получаем запрос на поиск фильмов
-if (isset($_GET['movie-search-input'])) {
-    $movie_name = $_GET['movie-search-input'];
-
-    // Создаем объект для отправки запросов
-    $client = new Client(['base_uri' => 'https://api.themoviedb.org/3/']);
-
-    // Отправляем запрос к The Movie Database API
-    $response = $client->request('GET', 'search/movie', [
-        'query' => [
-            'api_key' => $api_key,
-            'query' => $movie_name,
-        ],
-    ]);
-
-    // Получаем результаты поиска
-    $movies = json_decode($response->getBody(), true);
-
-    // Выводим результаты на страницу
-    $html = '<ul>';
-    foreach ($movies['results'] as $movie) {
-        $html .= '<li><a href="movie.php?id=' . $movie['id'] . '">' . $movie['title'] . '</a></li>';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $search = $_POST['search'];
+  
+  // Создаем клиент GuzzleHttp для отправки запросов к API The Movie Database
+  $client = new Client([
+    'base_uri' => 'https://api.themoviedb.org/3/',
+  ]);
+  
+  // Формируем URL-адрес для запроса поиска фильмов
+  $url = 'search/movie?api_key=fca80a35e9a4bccbf9a300c8e938e3e0&query=' . urlencode($search);
+  
+  // Отправляем запрос к API и получаем ответ
+  $response = $client->request('GET', $url);
+  $data = json_decode($response->getBody()->getContents(), true);
+  
+  // Получаем список найденных фильмов
+  $movies = $data['results'];
+  
+  // Выводим список найденных фильмов
+  if (!empty($movies)) {
+    echo '<ul>';
+    foreach ($movies as $movie) {
+      echo '<li>';
+      echo '<input type="checkbox" name="movies[]" value="' . $movie['id'] . '"> ';
+      echo $movie['title'];
+      echo '</li>';
     }
-    $html .= '</ul>';
-    echo $html;
+    echo '</ul>';
+    echo '<button type="submit">Add selected movies</button>';
+  } else {
+    echo 'No movies found.';
   }
+}
+
+
+
 
   
 
@@ -93,59 +102,3 @@ echo '</form>';
 
 echo '<a href="logout.php">Logout</a>';
 ?>
-
-
-<!-- HTML-код страницы профиля -->
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>User Profile</title>
-</head>
-<body>
-  <h1>User Profile</h1>
-  <form>
-    <label for="search">Search for movies:</label>
-    <input type="text" id="search" name="search">
-    <ul id="movies"></ul>
-    <button type="submit">Add selected movies</button>
-  </form>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-    $(function() {
-      // При изменении текста в поле поиска отправляем AJAX-запрос на сервер
-      $('#search').on('input', function() {
-        var query = $(this).val();
-        $.ajax({
-          url: 'search.php',
-          data: { q: query },
-          dataType: 'json',
-          success: function(data) {
-            // Очищаем список найденных фильмов
-            $('#movies').empty();
-            // Если найдены фильмы, добавляем их в список
-            if (data.length > 0) {
-              $.each(data, function(index, movie) {
-                var checkbox = $('<input>', {
-                  type: 'checkbox',
-                  name: 'movies[]',
-                  value: movie.id
-                });
-                var label = $('<label>').text(movie.title);
-                var li = $('<li>').append(checkbox).append(label);
-                $('#movies').append(li);
-              });
-            } else {
-              // Если фильмы не найдены, выводим соответствующее сообщение
-              var li = $('<li>').text('No movies found.');
-              $('#movies').append(li);
-            }
-          },
-          error: function() {
-            console.log('An error occurred while processing the request.');
-          }
-        });
-      });
-    });
-  </script>
-</body>
-</html>
